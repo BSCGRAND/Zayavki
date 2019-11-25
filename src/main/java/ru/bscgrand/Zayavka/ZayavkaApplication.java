a18b1d42a54198e84ca6438dafeffd30fd81b373
Anton Tsyakrev <oxmo.v3@yandex.ru>
2019-11-25 10:04:51 +0700

    Initial commit

---
+++ src/main/java/ru/bscgrand/Zayavka/ZayavkaApplication.java
@@ -0,0 +1,13 @@
+package ru.bscgrand.Zayavka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZayavkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZayavkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bscgrand/Zayavka/ZayavkaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bscgrand.Zayavka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZayavkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
