e5b227a5795d3f991212b88a199c286b4d494537
Anton Tsyakrev <oxmo.v3@yandex.ru>
2019-11-25 15:17:04 +0700

    Скелет для примера

--- src/main/java/ru/bscgrand/Zayavka/Models/UserRepository.java
+++ src/main/java/ru/bscgrand/Zayavka/Models/UserRepository.java
@@ -2,7 +2,9 @@ package ru.bscgrand.Zayavka.Models;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<Users,Long> {
 
 }
