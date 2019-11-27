cd87c3d3baceed535d3fe08d9bdc52bc379d2e3d
Andrei <amoskvin87@gmail.com>
2019-11-27 09:48:04 +0700

    add scanForExel controller

--- src/main/java/ru/bscgrand/Zayavka/Controllers/RequestController.java
+++ src/main/java/ru/bscgrand/Zayavka/Controllers/RequestController.java
@@ -4,7 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ru.bscgrand.Zayavka.Models.GoodsRequest;
 import ru.bscgrand.Zayavka.Models.GoodsRequestRepository;
+import ru.bscgrand.Zayavka.services.exelHandling.CopyFromExel;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -45,4 +51,21 @@ public class RequestController {
     // Добавить заявку из экселя
 //    @PutMapping("/add")
 //    public String putGoodsRequest()
+
+    @PutMapping("/scan")
+    public String scanForUpdate() {
+        String dateString = "18 11 2019";
+        SimpleDateFormat df = new SimpleDateFormat("yyyy MM dd");
+        Date date = null;
+        try {
+            date = df.parse(dateString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        List<GoodsRequest> addNewGoodsRequests = new CopyFromExel().addNewGoodsRequests(date);
+        for (GoodsRequest gr: addNewGoodsRequests) {
+            goodsRequestRepository.save(gr);
+        }
+        return "REQUESTS UPDATE SUCCESS";
+    }
 }
